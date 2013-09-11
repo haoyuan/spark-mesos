496f9a93a88b4b82af90f4d8d2a84f5661d0f9a6
Haoyuan Li <haoyuan@cs.berkeley.edu>
2013-09-10 21:29:42 -0700

    add basic perf

---
+++ examples/src/main/scala/org/apache/spark/examples/BasicPerf.scala
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.examples
+
+import scala.math.random
+import org.apache.spark._
+import SparkContext._
+
+/** Computes an approximation to pi */
+object BasicPerf {
+  def main(args: Array[String]) {
+    if (args.length != 4) {
+      System.err.println("Usage: BasicPerf <MasterAddr> <DataPath> <Count|Filter|WC]> <Tachyon|SerCache|DeCache>")
+      System.exit(1)
+    }
+
+    // Setup
+    val outputdata = args(1) + "-" + args(2) + "-" + args(3)
+    val sc = new SparkContext(args(0), "BasicPerf " + outputdata,
+      System.getenv("SPARK_HOME"), Seq(System.getenv("SPARK_EXAMPLES_JAR")))
+
+    // Warmup
+    val WARMUP_NUM = 500
+    val warm = sc.parallelize(1 to WARMUP_NUM, WARMUP_NUM).map(i => {
+        var sum = 0
+        for (i <- 0 until WARMUP_NUM) {
+          sum += i
+        }
+        sum
+      }).collect()
+    println("Just warmed up.")
+
+    val data = sc.textFile(args(1))
+    if (args(3).equal("Tachyon")) {
+    } else if (args(3).equal("SerCache"))
+      data.persist()
+    } else if (args(3).equal("DeCache")) {
+      data.persist(org.apache.spark.storage.StorageLevel.MEMORY_ONLY_SER)
+    } else {
+      System.err.println("Usage: BasicPerf <MasterAddr> <DataPath> <Count|Filter|WC]> <Tachyon|SerCache|DeCache>")
+      System.err.println("Wrong Cache Type: " + args(3))
+      System.exit(1)
+    }
+    data.count()
+    // Done setup
+
+    val firstStartTimeMs = System.currentTimeMillis()
+    var startTimeMs = System.currentTimeMillis()
+    if (args(2).equal("Count")) {
+      for (i <- 1 to 10) {
+        startTimeMs = System.currentTimeMillis()
+        data.count()
+        System.out.println("Round Time " + i + " : " + (System.currentTimeMillis() - startTimeMs) + " ms.")
+      }
+      System.out.println("Final Round Time " + " : " + (System.currentTimeMillis() - firstStartTimeMs) + " ms.")
+    } else if (args(2).equal("Filter")) {
+    } else if (args(2).equal("WC")) {
+      for (i <- 1 to 10) {
+        startTimeMs = System.currentTimeMillis()
+        data.flatMap(line => line.split(" ")).map(word => (word, i)).reduceByKey(_ + _).saveAsTextFile(outputdata + "-" + i)
+        System.out.println("Round Time " + i + " : " + (System.currentTimeMillis() - startTimeMs) + " ms.")
+      }
+      System.out.println("Final Round Time " + " : " + (System.currentTimeMillis() - firstStartTimeMs) + " ms.")
+    } else {
+      System.err.println("Usage: BasicPerf <MasterAddr> <DataPath> <Count|Filter|WC]> <Tachyon|SerCache|DeCache>")
+      System.err.println("Wrong Application Type: " + args(2))
+      System.exit(1)
+    }
+
+    System.exit(0)
+  }
+}
